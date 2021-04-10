d7bbcecefbfc9effbd78e7e1494c06b3e8657b8e
FatmaElkhateeb <61888582+FatmaElkhateeb@users.noreply.github.com>
2021-04-10 13:30:55 +0200

    Frist Commit
    change style text

---
+++ text.js
@@ -0,0 +1,48 @@
+var txt=document.getElementById("PAR");
+
+function ChangeFont(fn){
+    txt.style.fontFamily = fn;
+}
+
+function ChangeAlign(fn){
+    txt.style.textAlign=fn;
+}
+
+function ChangeHeight(fn){
+    txt.style.lineHeight=fn;
+}
+
+function ChangeLSpace(fn){
+    txt.style.letterSpacing=fn;
+
+}
+
+function ChangeLSpace(fn){
+    txt.style.letterSpacing=fn;
+
+}
+
+function ChangeIndent(fn){
+    txt.style.textIndent=fn;    
+}
+
+function ChangeTransform(fn)
+
+{
+    txt.style.textTransform=fn;
+}
+
+function ChangeDecorate(fn)
+
+{
+    txt.style.textDecoration=fn;
+}
+
+function ChangeBorder(fn){
+    txt.style.borderStyle=fn;
+}
+
+function ChangeBorderColor(fn){
+    txt.style.borderColor=fn;
+
+}
